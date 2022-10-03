b43c1a8a89fb0aa0d6f76284bf5cce280f9fafc8
Daniel Castillo <97797577+Dcextremo@users.noreply.github.com>
2022-10-03 10:17:11 -0300

    Lista de items

---
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit , Input} from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TASKS } from 'src/app/mock-tasks'; 
+
+@Component({
+  selector: 'app-tasks-item',
+  templateUrl: './tasks-item.component.html',
+  styleUrls: ['./tasks-item.component.css']
+})
+export class TasksItemComponent implements OnInit {
+
+
+  @Input() task: Task = TASKS[0]
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { TASKS } from 'src/app/mock-tasks';
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+
+  tasks: Task[] = TASKS;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
