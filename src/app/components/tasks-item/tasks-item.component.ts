dc92e31c29cb90ae3e130169916db819c5775072
Daniel Castillo <97797577+Dcextremo@users.noreply.github.com>
2022-10-03 16:45:56 -0300

    servicios on-off

--- src/app/components/tasks-item/tasks-item.component.ts
+++ src/app/components/tasks-item/tasks-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit , Input} from '@angular/core';
+import { Component, OnInit , Input, Output, EventEmitter } from '@angular/core';
 import { Task } from 'src/app/Task';
 import { TASKS } from 'src/app/mock-tasks';
 
@@ -13,10 +13,20 @@ export class TasksItemComponent implements OnInit {
 
 
   @Input() task: Task = TASKS[0]
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter() 
+  @Output() onToggleReminder: EventEmitter<Task>= new EventEmitter()
   faTimes= faTimes;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onDelete(task: Task)
+  {
+    this.onDeleteTask.emit(task)
+  }
+  onToggle(task: Task)
+  {
+    this.onToggleReminder.emit(task);
+  }
 }
--- src/app/service/task.service.ts
+++ src/app/service/task.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpHandler} from '@angular/common/http'
+import {HttpClient, HttpHandler, HttpHeaders} from '@angular/common/http'
 import { Observable, observable, of } from 'rxjs';
 import { Task } from '../Task'; 
 import { TASKS } from '../mock-tasks';
 
+const httpOptions=
+{
+  headers: new HttpHeaders(
+    {'Content-Type':'application/json'}
+  )
+}
 @Injectable({
   providedIn: 'root'
 })
@@ -18,5 +24,15 @@ export class TaskService {
   getTasks(): Observable <Task[]>
   {
       return this.http.get<Task[]>(this.apiUrl)
+  }
+  deleteTask(task:Task):Observable<Task>
+  {
+    const url =`${this.apiUrl}/${task.id}`
+    return this.http.delete<Task>(url)
+  }
+  updateTaskReminder(task:Task): Observable<Task>
+  {
+    const url =`${this.apiUrl}/${task.id}`
+    return this.http.put<Task>(url, task , httpOptions)
   }
 }
